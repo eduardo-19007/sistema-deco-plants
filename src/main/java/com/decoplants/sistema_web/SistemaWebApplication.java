3f21153d2911049cf75bba325fd6779d8b48853f
Eduardo <kerl.grone@gmail.com>
2026-07-10 12:11:42 -0500

    actualizacion de seguridad

--- src/main/java/com/decoplants/sistema_web/SistemaWebApplication.java
+++ src/main/java/com/decoplants/sistema_web/SistemaWebApplication.java
@@ -3,7 +3,6 @@ package com.decoplants.sistema_web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;   
 
-//Clase principal (Main) del sistema.
 
 @SpringBootApplication 
 public class SistemaWebApplication {
